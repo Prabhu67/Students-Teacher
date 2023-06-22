59404990ef39fd882e0518b0e327d7a6aaa2db8f
Prabhu67 <p.prabhu.pb@gmail.com>
2023-06-22 22:00:34 +0530

    First Commit

---
+++ src/Data/Students_data.js
@@ -0,0 +1,50 @@
+var data =  [
+  {
+  name : "Prabhu",
+  gender : "male",
+  batch: "B45WD",
+  qualification : "BBA"
+},
+{
+  name: "Barathkumar",
+  gender: "male",
+  batch: "B45WD",
+  qualification: "B.E-EEE"
+},
+{
+  name: "Anitha",
+  gender: "female",
+  batch: "B45WD",
+  qualification: "BSC"
+},
+{
+  name: "Jeeva",
+  gender: "male",
+  batch: "B45WD",
+  qualification: "B.E-Mech"
+},
+{
+  name: "Anirutha",
+  gender: "female",
+  batch: "B45WD",
+  qualification: "BCA"
+},
+{
+  name: "Kavin",
+  gender: "male",
+  batch: "B45WD",
+  qualification: "B.E-Mech"
+},{
+  name: "Mahesh",
+  gender: "male",
+  batch: "-",
+  qualification: "B.E-Mech"
+},
+{
+  name: "Kumar",
+  gender: "male",
+  batch: "-",
+  qualification: "B.E-Mech"
+}
+];
+export default data;
